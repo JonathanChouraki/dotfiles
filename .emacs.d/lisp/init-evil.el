382ee7d86cf9358866ffe61bbf7e1a72e3b00f0b
Jonathan Chouraki <jonathan.chouraki@gmail.com>
2016-07-25 13:01:51 -0400

    Added keybind to open and move around splits

--- .emacs.d/lisp/init-evil.el
+++ .emacs.d/lisp/init-evil.el
@@ -1,23 +1,36 @@
-(defun jc/my-evil-config ()
+(defun my/evil-config ()
   "my evil configuration"
     ;; Make escape quit everything, whenever possible.
-    (define-key evil-normal-state-map [escape] 'keyboard-quit)
-    (define-key evil-visual-state-map [escape] 'keyboard-quit)
-    (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
-    (define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
-    (define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
-    (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
-    (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit))
+    ;(define-key evil-normal-state-map [escape] 'keyboard-quit)
+    ;(define-key evil-visual-state-map [escape] 'keyboard-quit)
+    ;(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+    ;(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+    ;(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+    ;(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+    ;(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+    (define-key evil-normal-state-map (kbd "gl") 'windmove-right)
+    (define-key evil-normal-state-map (kbd "gh") 'windmove-left)
+    (define-key evil-normal-state-map (kbd "gk") 'windmove-up)
+    (define-key evil-normal-state-map (kbd "gj") 'windmove-down))
+
+
+(defun my/evil-leader-config ()
+  "my evil leader configuration"
+    (evil-leader/set-leader ",")
+    (evil-leader/set-key "s" 'split-window-right)
+    (evil-leader/set-key "h" 'split-window-below))
+
+(use-package evil-leader
+    :ensure t
+    :config
+    (global-evil-leader-mode)
+    (my/evil-leader-config))
 
 (use-package evil
   :ensure t
   :config
   (evil-mode 1)
-
-  (use-package evil-leader
-    :ensure t
-    :config
-    (global-evil-leader-mode))
+  (my/evil-config)
 
   (use-package evil-surround
     :ensure t
