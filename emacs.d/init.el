491db02dcb82b3fe087d8e04c64cfac4f5f442f9
Jonathan Chouraki <jonathan.chouraki@gmail.com>
2017-03-19 21:02:52 -0400

    Added elscreen configuration

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -18,3 +18,23 @@
 (require 'init-global)
 (require 'init-php)
 (require 'init-evil)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elscreen-tab-background-face ((t nil))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c616e584f7268aa3b63d08045a912b50863a34e7ea83e35fcab8537b75741956" default)))
+ '(elscreen-display-screen-number t)
+ '(elscreen-tab-background-face ((t nil)))
+ '(elscreen-tab-current-screen-face ((t (:background "gray10" :foreground "gray90"))))
+ '(elscreen-tab-display-control nil)
+ '(elscreen-tab-display-kill-screen nil)
+ '(elscreen-tab-other-screen-face ((t (:background "gray40" :foreground "gray90")))))
