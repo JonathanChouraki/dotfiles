feacc61e24f860a8d38d05828e5a2cb6b74db264
Jonathan Chouraki <jonathan.chouraki@gmail.com>
2017-04-08 16:16:03 -0400

    Added identity configuration

--- emacs.d/lisp/init-global.el
+++ emacs.d/lisp/init-global.el
@@ -33,6 +33,9 @@
   :config
   (load-theme 'airline-kolor))
 
+(setq user-full-name "Jonathan Chouraki"
+      user-mail-address "jonathan.chouraki@gmail.com")
+
 (set-frame-font "Fira Mono for Powerline 11")
 (setq inhibit-splash-screen t
       inhibit-startup-message t
@@ -41,6 +44,15 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (show-paren-mode 1)
+
+(setq
+   backup-by-copying t
+   backup-directory-alist
+    '(("." . "~/.saves"))
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)
 
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4) 
